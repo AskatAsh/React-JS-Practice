a6bc77fbe5e5d11cb034dc1ad35ea987362bc761
AskatAsh <abdulaskat@gmail.com>
2023-10-27 19:32:45 +0600

    used curly braces and parenthesis to return jsx

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -2,38 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 function MyButton(){
   return (
     <button>Click Here</button>
   );
 }
+function Name(){
+  const age = 25;
+  return <h3>I am Askat. I am {age} years old.</h3>
+}
+function Student(){
+  const std = {name: "Askat", Id: 123};
+  return (
+  <div>
+    <p>Name: {std.name}</p>
+    <p>ID: {std.Id}</p>
+  </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <MyButton />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World</h1>
+      <Name></Name>
+      <MyButton />
+      <Student></Student>
     </>
   )
 }
