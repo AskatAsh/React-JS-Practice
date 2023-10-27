3bc166dcfc653184c71741488221417f5df9c875
AskatAsh <abdulaskat@gmail.com>
2023-10-27 19:47:52 +0600

    used object to create styles in function

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -13,9 +13,16 @@ function Name(){
   return <h3>I am Askat. I am {age} years old.</h3>
 }
 function Student(){
+  const stdStyles = {
+    border: '2px solid #fff',
+    borderRadius: '8px',
+    padding: '15px',
+    marginTop: '20px',
+    backgroundColor: '#101010'
+  }
   const std = {name: "Askat", Id: 123};
   return (
-  <div>
+  <div style={stdStyles}>
     <p>Name: {std.name}</p>
     <p>ID: {std.Id}</p>
   </div>
