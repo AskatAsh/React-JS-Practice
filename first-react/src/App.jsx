262d23e1ed39c4e6d0b8f85d31e70471d6e24d84
AskatAsh <abdulaskat@gmail.com>
2023-10-27 23:59:05 +0600

    passed object as props

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -3,16 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function MyButton(){
+function MyButton() {
   return (
     <button className='btn'>Click Here</button>
   );
 }
-function Name(){
+function Name() {
   const age = 25;
   return <h3>I am Askat. I am {age} years old.</h3>
 }
-function Student(){
+function Student() {
   const stdStyles = {
     border: '2px solid #fff',
     borderRadius: '8px',
@@ -20,19 +20,27 @@ function Student(){
     marginTop: '20px',
     backgroundColor: '#101010'
   }
-  const std = {name: "Askat", Id: 123};
+  const std = { name: "Askat", Id: 123 };
   return (
-  <div style={stdStyles}>
-    <p>Name: {std.name}</p>
-    <p>ID: {std.Id}</p>
-  </div>
+    <div style={stdStyles}>
+      <p>Name: {std.name}</p>
+      <p>ID: {std.Id}</p>
+    </div>
   )
 }
 
-function Device(props){
+function Device(props) {
   return <h4>Device Name: {props.deviceName}, Color: {props.color}</h4>
 }
 
+function UserData({user}) {
+  return (
+    <div>
+      <p>User Name: {user.userName}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  )
+}
 function App() {
   return (
     <>
@@ -42,6 +50,7 @@ function App() {
       <Student></Student>
       <Device deviceName="Mobile" color="Green"></Device>
       <Device deviceName="Laptop" color="Silver"></Device>
+      <UserData user={{ userName: "Askat", email: "abc@example.com" }}></UserData>
     </>
   )
 }
