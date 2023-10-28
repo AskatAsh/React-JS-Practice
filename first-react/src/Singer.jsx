9db61ba5e2ac29bea571c198bb0e57e92cfdc579
AskatAsh <abdulaskat@gmail.com>
2023-10-28 14:49:53 +0600

    used external css and imported css file

--- first-react/src/Singer.jsx
+++ first-react/src/Singer.jsx
@@ -1,8 +1,9 @@
+import './Singer.css'
 export default function Singer({singer}){
     // console.log(singer);
     const {name, age} = singer;
     return (
-        <div>
+        <div className='singer'>
             <h3>Singer: {name}</h3>
             <p>Age: {age}</p>
         </div>
