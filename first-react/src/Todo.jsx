e7066f8f102be546e63d8dd82fb329cd293ae131
AskatAsh <abdulaskat@gmail.com>
2023-10-28 12:23:25 +0600

    used && and || operator for conditional rendering

--- first-react/src/Todo.jsx
+++ first-react/src/Todo.jsx
@@ -7,8 +7,13 @@
 //     }
 // }
 
+// export default function Todo({task, isDone}){
+//         return <li>{isDone ? "finished" : "ON going"}: {task}</li>
+// }
+
+// export default function Todo({task, isDone}){
+//     return <li>{task} {isDone && ": Done"}</li>
+// }
 export default function Todo({task, isDone}){
-    if(isDone){
-        return <li>{isDone ? "finished" : "ON going"}: {task}</li>
-    }
+    return <li>{task} {isDone || ": On Going"}</li>
 }
