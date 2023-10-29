ed2a5c4da2a23424a5f7dacbc3416313a98a4d51
AskatAsh <abdulaskat@gmail.com>
2023-10-30 01:52:31 +0600

    used external css by importing

--- second-react/src/Friend.jsx
+++ second-react/src/Friend.jsx
@@ -1,15 +1,17 @@
+import './Friends.css'
 export default function Friend({friend}){
-    const friendStyle = {
-        border: '2px solid lightgreen',
-        borderRadius: '10px',
-        padding: '10px',
-        margin: '10px'
-    }
+    const {name, email, website} = friend;
+    // const friendStyle = {
+    //     border: '2px solid lightgreen',
+    //     borderRadius: '10px',
+    //     padding: '10px',
+    //     margin: '10px'
+    // }
     return (
-        <div style={friendStyle}>
-            <h4>Name: {friend.name}</h4>
-            <h4>Email: {friend.email}</h4>
-            <h4>Website: {friend.website}</h4>
+        <div className='friends'>
+            <h4>Name: {name}</h4>
+            <h4>Email: {email}</h4>
+            <h4>Website: {website}</h4>
         </div>
     )
 }
