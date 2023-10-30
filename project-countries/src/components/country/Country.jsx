afd2ae371d3f653fa05d91d61535d8da20626b48
AskatAsh <abdulaskat@gmail.com>
2023-10-31 00:05:12 +0600

    used conditional statement to get languages of countries

--- project-countries/src/components/country/Country.jsx
+++ project-countries/src/components/country/Country.jsx
@@ -1,14 +1,21 @@
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
     // console.log(country);
     // const {name: {common}, flags} = country;
-    const {name, flags, capital, population} = country;
+    const { name, flags, capital, population, languages } = country;
+
+    if (!languages) {
+        return null;
+    }
+    const language = Object.values(languages);
+    console.log(language);
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
-            <img src={flags.png} alt={flags.alt} className='flag'/>
+            <img src={flags.png} alt={flags.alt} className='flag' />
             <p><b>Capital:</b> {capital}, <b>Population:</b> {population}</p>
+            <p><b>Language: </b>{language[0]}</p>
         </div>
     );
 };
