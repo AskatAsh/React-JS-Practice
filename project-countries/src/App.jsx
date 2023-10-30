92687c0a0c3444d0fc52af317ee9d05b87aa98d0
AskatAsh <abdulaskat@gmail.com>
2023-10-30 19:59:09 +0600

    removed unused variables and imports

--- project-countries/src/App.jsx
+++ project-countries/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h3>React Countries</h3>
+      <h3>Countries in the World</h3>
     </>
   )
 }
