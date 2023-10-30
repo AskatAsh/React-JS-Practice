96a1753cbcf26457fc784022e97a9d7d8b1143f6
AskatAsh <abdulaskat@gmail.com>
2023-10-30 20:54:10 +0600

    used destructuring to show flags of countries

--- project-countries/src/components/country/Country.jsx
+++ project-countries/src/components/country/Country.jsx
@@ -2,10 +2,11 @@ import './Country.css';
 
 const Country = ({country}) => {
     console.log(country);
-    const {name: {common}} = country;
+    const {name: {common}, flags} = country;
     return (
         <div className='country'>
             <h3>Name: {common}</h3>
+            <img src={flags.png} alt={flags.alt} />
         </div>
     );
 };
