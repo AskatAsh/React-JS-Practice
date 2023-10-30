44505fab6ab6cbea4545b0b6be0bbfed43f02ce5
AskatAsh <abdulaskat@gmail.com>
2023-10-30 22:24:49 +0600

    showed capital and population from countries API

--- project-countries/src/components/country/Country.jsx
+++ project-countries/src/components/country/Country.jsx
@@ -1,13 +1,14 @@
 import './Country.css';
 
 const Country = ({country}) => {
-    console.log(country);
+    // console.log(country);
     // const {name: {common}, flags} = country;
-    const {name, flags} = country;
+    const {name, flags, capital, population} = country;
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt={flags.alt} />
+            <p><b>Capital:</b> {capital}, <b>Population:</b> {population}</p>
         </div>
     );
 };
