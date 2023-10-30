4d1757890079607048425e1f3a73e0ccf0fdd327
AskatAsh <abdulaskat@gmail.com>
2023-10-30 10:00:59 +0600

    used useState and useEffect to get data from API and store it

--- second-react/src/App.jsx
+++ second-react/src/App.jsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
 import Team from './Team'
 import Users from './Users'
 import Friends from './Friends'
+import Posts from './Posts'
 
 function App() {
-  function handleClick(){
-    alert("clicked on the button");
-  }
-  function addToFive(num){
-    alert(num+5);
-  }
+  // function handleClick(){
+  //   alert("clicked on the button");
+  // }
+  // function addToFive(num){
+  //   alert(num+5);
+  // }
   return (
     <>
       <h2>React Core Concepts</h2>
-      <Friends></Friends>
-      <Users></Users>
+      <Posts></Posts>
+      {/* <Friends></Friends> */}
+      {/* <Users></Users>
       <Team></Team>
       <Counter></Counter>
       <button onClick={handleClick}>Click Here</button>
-      <button onClick={() => addToFive(7)}>Click to Add</button>
+      <button onClick={() => addToFive(7)}>Click to Add</button> */}
     </>
   )
 }
