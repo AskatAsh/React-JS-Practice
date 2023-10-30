fe6eaa1a047a9fb58e98fb82607e7846cae43df2
AskatAsh <abdulaskat@gmail.com>
2023-10-30 20:43:31 +0600

    used object destructuring to show country name

--- project-countries/src/components/country/Country.jsx
+++ project-countries/src/components/country/Country.jsx
@@ -1,8 +1,9 @@
 const Country = ({country}) => {
     console.log(country);
+    const {name: {common}} = country;
     return (
         <div>
-            
+            <h3>Name: {common}</h3>
         </div>
     );
 };
