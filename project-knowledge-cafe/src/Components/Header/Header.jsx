e2517e6a25a9a933529a275f3d5f4e78cc6f203d
AskatAsh <abdulaskat@gmail.com>
2023-11-01 14:32:56 +0600

    created images folder with an image file and showed in header

--- project-knowledge-cafe/src/Components/Header/Header.jsx
+++ project-knowledge-cafe/src/Components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from '../../assets/Images/profile.png'
 const Header = () => {
     return (
         <div>
             <h1 className="text-3xl font-bold">React Knowledge Cafe</h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
