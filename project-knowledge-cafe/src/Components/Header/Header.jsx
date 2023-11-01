0233301cd98456368b20aff2701b571b69884be5
AskatAsh <abdulaskat@gmail.com>
2023-11-01 15:30:31 +0600

    added style to header and alt text in image

--- project-knowledge-cafe/src/Components/Header/Header.jsx
+++ project-knowledge-cafe/src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import profile from '../../assets/Images/profile.png'
 const Header = () => {
     return (
-        <div>
+        <div className='flex items-center justify-between'>
             <h1 className="text-3xl font-bold">React Knowledge Cafe</h1>
-            <img src={profile} alt="" />
+            <img src={profile} alt="User profile picture" />
         </div>
     );
 };
