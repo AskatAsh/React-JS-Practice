7ffd5d6ba2de30f64d742934d99355cb456a1814
AskatAsh <abdulaskat@gmail.com>
2023-11-03 16:30:29 +0600

    created a custom navbar with fake routes data

--- third-react/src/App.jsx
+++ third-react/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-import NavBar from './Component/Header/NavBar'
+import CustomNav from './Component/Custom/CustomNav'
+// import NavBar from './Component/Header/NavBar'
 
 function App() {
 
   return (
     <>
-
-      <NavBar></NavBar>
+      <CustomNav></CustomNav>
+      {/* <NavBar></NavBar> */}
       <h1 className='text-5xl font-bold text-center mt-10'>React Third App</h1>
 
     </>
--- third-react/src/Component/Custom/CustomNav.jsx
+++ third-react/src/Component/Custom/CustomNav.jsx
@@ -1,7 +1,19 @@
 const CustomNav = () => {
+    const routesData = [
+        { id: 1, name: "Home", path: "/" },
+        { id: 2, name: "About", path: "/about" },
+        { id: 3, name: "Services", path: "/services" },
+        { id: 4, name: "Products", path: "/products" },
+        { id: 5, name: "Contact", path: "/contact" }
+    ];
+
     return (
         <nav>
-            
+            <ul>
+                {
+                    routesData.map(route => <li key={route.id}><a href={route.path}>{route.name}</a></li>)
+                }
+            </ul>
         </nav>
     );
 };
