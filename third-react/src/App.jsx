18e89f2a2af128843f050d0041f3403e2335a736
AskatAsh <abdulaskat@gmail.com>
2023-11-03 16:06:52 +0600

    tested daisyUI working with hero section jsx

--- third-react/src/App.jsx
+++ third-react/src/App.jsx
@@ -4,9 +4,19 @@ function App() {
 
   return (
     <>
-      
-      <h1 className='text-4xl font-bold'>React Third App</h1>
-      
+
+      <h1 className='text-5xl font-bold text-center'>React Third App</h1>
+
+      <div className="hero mt-12">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-4xl font-bold">Hello there</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
