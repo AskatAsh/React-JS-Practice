2bd276f55111a3102031d66b984541dcfe875396
AskatAsh <abdulaskat@gmail.com>
2023-11-05 20:02:34 +0600

    added flex-grow in ul and margin in button

--- third-react/src/Component/PriceOption/PriceOption.jsx
+++ third-react/src/Component/PriceOption/PriceOption.jsx
@@ -7,12 +7,12 @@ function PriceOption({option}) {
         <div className='border-2 border-gray-800 rounded-md shadow-md p-5 flex flex-col items-center gap-2'>
             <h3 className='text-2xl font-semibold'>{name}</h3>
             <h4><span className='text-4xl font-bold'>{price}</span>/month</h4>
-            <ul className='pl-5 pt-5'>
+            <ul className='pl-3 pt-5 flex-grow'>
                 {
                     features.map((feature, idx) => <Features key={idx} feature={feature}></Features>)
                 }
             </ul>
-            <button className='btn bg-gray-800 text-white font-bold tracking-wider'>Purchase</button>
+            <button className='btn bg-gray-800 text-white font-bold tracking-wider my-5 w-full'>Purchase</button>
         </div>
     );
 }
