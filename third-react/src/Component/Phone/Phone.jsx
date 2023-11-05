697edd41f5a398ce18ad68644b8ff3dc139bea0c
AskatAsh <abdulaskat@gmail.com>
2023-11-06 01:01:18 +0600

    created fake object data of phone name and price using map

--- third-react/src/Component/Phone/Phone.jsx
+++ third-react/src/Component/Phone/Phone.jsx
@@ -9,10 +9,21 @@ const Phone = () => {
     // }, [])
     axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
     // .then(data => console.log(data.data.data))
-    .then(data => setPhones(data.data.data))
+    .then(data => {
+        const phonesData = data.data.data;
+        const phonesFakeData = phonesData.map(phone => {
+            const obj = {
+                name: phone.phone_name,
+                price: parseInt(phone.slug.split('-')[1])
+            }
+            return obj;
+        })
+        // console.log(phonesFakeData);
+        setPhones(phonesFakeData);
+    })
     return (
         <div>
-            <h2 className="text-3xl">Phones: {phones.length}</h2>
+            <h2 className="text-3xl">Phones: </h2>
         </div>
     );
 };
