9cdc1a627c80f39cd36ca4acca838808126fb670
AskatAsh <abdulaskat@gmail.com>
2023-11-05 19:47:45 +0600

    added react check icon for features list

--- third-react/src/Component/Features/Features.jsx
+++ third-react/src/Component/Features/Features.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import { BsFillPatchCheckFill } from 'react-icons/bs';
 
 function Features({feature}) {
     return (
-        <li>{feature}</li>
+        <li className='flex items-center gap-2 py-2'><BsFillPatchCheckFill className='text-xl shrink-0'></BsFillPatchCheckFill> {feature}</li>
     );
 }
 
