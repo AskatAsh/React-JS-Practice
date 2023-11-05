f4dafdf282502c2f064cac737c3e732e8f478984
AskatAsh <abdulaskat@gmail.com>
2023-11-05 18:27:18 +0600

    created PriceOptions component and added to App.jsx

--- third-react/src/App.jsx
+++ third-react/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import CustomNav from './Component/Custom/CustomNav'
+import PriceOptions from './Component/PriceOptions/PriceOptions'
 // import NavBar from './Component/Header/NavBar'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       {/* <NavBar></NavBar> */}
       <h1 className='text-5xl font-bold text-center mt-10'>React Third App</h1>
 
+      <PriceOptions></PriceOptions>
     </>
   )
 }
---
+++ third-react/src/Component/PriceOptions/PriceOptions.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const PriceOptions = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+PriceOptions.propTypes = {
+    
+};
+
+export default PriceOptions;
