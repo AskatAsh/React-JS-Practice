3ade86afc42a3cfb38dbf1cfc2bbf7e2114835a9
AskatAsh <abdulaskat@gmail.com>
2023-11-06 01:32:35 +0600

    showing spinner on page loading

--- third-react/src/Component/Phone/Phone.jsx
+++ third-react/src/Component/Phone/Phone.jsx
@@ -7,6 +7,7 @@ import { ColorRing } from 'react-loader-spinner'
 
 const Phone = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
     // useEffect(() => {
     //     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     //     .then(res => res.json())
@@ -25,19 +26,20 @@ const Phone = () => {
             })
             // console.log(phonesFakeData);
             setPhones(phonesFakeData);
+            setLoading(false);
         })
 
     return (
         <div className='flex flex-col items-center my-20'>
-            <ColorRing
-                visible={true}
-                height="80"
-                width="80"
-                ariaLabel="blocks-loading"
-                wrapperStyle={{}}
-                wrapperClass="blocks-wrapper"
-                colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-            />
+        {loading && <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />}
 
             <h2 className="text-3xl font-semibold my-10">Phones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
