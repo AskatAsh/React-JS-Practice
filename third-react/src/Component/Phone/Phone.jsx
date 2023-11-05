f9ba2aa3e4e78cb4fabe249cf5516cbc971429fb
AskatAsh <abdulaskat@gmail.com>
2023-11-06 00:46:53 +0600

    get phone API data using axios

--- third-react/src/Component/Phone/Phone.jsx
+++ third-react/src/Component/Phone/Phone.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 const Phone = () => {
     const [phones, setPhones] = useState([]);
-    useEffect(() => {
-        fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(res => res.json())
-        .then(data => setPhones(data.data))
-    }, [])
-    // axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-    // .then(data => console.log(data.data))
+    // useEffect(() => {
+    //     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    //     .then(res => res.json())
+    //     .then(data => setPhones(data.data))
+    // }, [])
+    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+    // .then(data => console.log(data.data.data))
+    .then(data => setPhones(data.data.data))
     return (
         <div>
             <h2 className="text-3xl">Phones: {phones.length}</h2>
