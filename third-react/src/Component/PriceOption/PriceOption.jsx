fcea484071abcb13261ec8d38734fc25bde9ace4
AskatAsh <abdulaskat@gmail.com>
2023-11-05 19:53:54 +0600

    added purchase button with tailwind styles

--- third-react/src/Component/PriceOption/PriceOption.jsx
+++ third-react/src/Component/PriceOption/PriceOption.jsx
@@ -4,7 +4,7 @@ import Features from '../Features/Features';
 function PriceOption({option}) {
     const {name, price, features} = option;
     return (
-        <div className='border-2 border-gray-800 rounded-md shadow-md p-5'>
+        <div className='border-2 border-gray-800 rounded-md shadow-md p-5 flex flex-col items-center gap-2'>
             <h3 className='text-2xl font-semibold'>{name}</h3>
             <h4><span className='text-4xl font-bold'>{price}</span>/month</h4>
             <ul className='pl-5 pt-5'>
@@ -12,6 +12,7 @@ function PriceOption({option}) {
                     features.map((feature, idx) => <Features key={idx} feature={feature}></Features>)
                 }
             </ul>
+            <button className='btn bg-gray-800 text-white font-bold tracking-wider'>Purchase</button>
         </div>
     );
 }
