57a03ef47103636f9ad927e6a993f5627575294e
AskatAsh <abdulaskat@gmail.com>
2023-11-06 00:43:20 +0600

    showed the phones data length using state

--- third-react/src/Component/Phone/Phone.jsx
+++ third-react/src/Component/Phone/Phone.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
-
+import axios from "axios";
 const Phone = () => {
     const [phones, setPhones] = useState([]);
     useEffect(() => {
         fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => setPhones(data.data))
     }, [])
+    // axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+    // .then(data => console.log(data.data))
     return (
         <div>
-            
+            <h2 className="text-3xl">Phones: {phones.length}</h2>
         </div>
     );
 };
