809410aa67f36b328b391bd3a3cc8e3f8322ea52
AskatAsh <abdulaskat@gmail.com>
2023-11-05 18:36:15 +0600

    created new comopnent PriceOption and added proptypes

---
+++ third-react/src/Component/PriceOption/PriceOption.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+function PriceOption({option}) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+PriceOption.propTypes = {
+    option: PropTypes.object
+};
+
+export default PriceOption;
--- third-react/src/Component/PriceOptions/PriceOptions.jsx
+++ third-react/src/Component/PriceOptions/PriceOptions.jsx
@@ -1,15 +1,9 @@
-import PropTypes from 'prop-types';
-
 const PriceOptions = () => {
     return (
         <div>
             
         </div>
     );
-};
-
-PriceOptions.propTypes = {
-    
 };
 
 export default PriceOptions;
