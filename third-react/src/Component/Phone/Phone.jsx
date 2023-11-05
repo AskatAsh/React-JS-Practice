9a8ad9c0cf01980ff1a012bfca0d70a9cef567b3
AskatAsh <abdulaskat@gmail.com>
2023-11-06 01:15:09 +0600

    added cartesian grid to barchart

--- third-react/src/Component/Phone/Phone.jsx
+++ third-react/src/Component/Phone/Phone.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 
 const Phone = () => {
@@ -27,10 +27,11 @@ const Phone = () => {
 
     return (
         <div className='flex flex-col items-center my-20'>
-            <h2 className="text-3xl">Phones: {phones.length}</h2>
-            <BarChart width={600} height={400} data={phones}>
+            <h2 className="text-3xl font-semibold my-10">Phones: {phones.length}</h2>
+            <BarChart width={700} height={400} data={phones}>
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <Bar dataKey="price" fill="#8884d8" />
-                <XAxis dataKey={'name'}/>
+                <XAxis dataKey={'name'} />
                 <YAxis />
                 <Tooltip />
             </BarChart>
