9deea389c3103fb1e46a517bc10a1b7fb7179d4b
AskatAsh <abdulaskat@gmail.com>
2023-11-05 19:27:10 +0600

    added tailwind style to ul in features list

--- third-react/src/Component/PriceOption/PriceOption.jsx
+++ third-react/src/Component/PriceOption/PriceOption.jsx
@@ -7,7 +7,7 @@ function PriceOption({option}) {
         <div className='border-2 border-gray-800 rounded-md shadow-md p-5'>
             <h3 className='text-2xl font-semibold'>{name}</h3>
             <h4><span className='text-4xl font-bold'>{price}</span>/month</h4>
-            <ul>
+            <ul className='list-disc pl-5 pt-5'>
                 {
                     features.map((feature, idx) => <Features key={idx} feature={feature}></Features>)
                 }
