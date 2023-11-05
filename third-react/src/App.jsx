a9bc005458bfd14900b97d289d084c593558c63f
AskatAsh <abdulaskat@gmail.com>
2023-11-05 20:31:55 +0600

    added linechart component to app.jsx

--- third-react/src/App.jsx
+++ third-react/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import CustomNav from './Component/Custom/CustomNav'
+import LineChart from './Component/LineChart/LineChart'
 import PriceOptions from './Component/PriceOptions/PriceOptions'
 // import NavBar from './Component/Header/NavBar'
 
@@ -12,6 +13,7 @@ function App() {
       {/* <h1 className='text-5xl font-bold text-center mt-10'>React Third App</h1> */}
 
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
     </>
   )
 }
--- third-react/src/Component/LineChart/LineChart.jsx
+++ third-react/src/Component/LineChart/LineChart.jsx
@@ -1,12 +1,24 @@
 import { LineChart as LChart, Line } from 'recharts';
 
 const LineChart = () => {
+
+    const studentMarks = [
+        { id: 1, name: "Student 1", mark: 85 },
+        { id: 2, name: "Student 2", mark: 92 },
+        { id: 3, name: "Student 3", mark: 78 },
+        { id: 4, name: "Student 4", mark: 88 },
+        { id: 5, name: "Student 5", mark: 95 },
+        { id: 6, name: "Student 6", mark: 76 },
+        { id: 7, name: "Student 7", mark: 90 },
+        { id: 8, name: "Student 8", mark: 84 },
+        { id: 9, name: "Student 9", mark: 89 },
+        { id: 10, name: "Student 10", mark: 93 },
+      ];
+      
     return (
-        <div>
-            <LChart width={400} height={400} data={data}>
-                <Line type="monotone" dataKey="uv" stroke="#8884d8"></Line>
+            <LChart width={500} height={400} data={studentMarks}>
+                <Line type="monotone" dataKey="id" stroke="#272727"></Line>
             </LChart>
-        </div>
     );
 };
 
