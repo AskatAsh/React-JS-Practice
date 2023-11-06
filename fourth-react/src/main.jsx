7a892315c5ddf8b9a448fce3b1377463be1ab173
AskatAsh <abdulaskat@gmail.com>
2023-11-06 11:40:49 +0600

    created react router for about and contact page

--- fourth-react/src/main.jsx
+++ fourth-react/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello from react router</div>
+  },
+  {
+    path: '/about',
+    element: <div>About page of react router</div>
+  },
+  {
+    path: '/contact',
+    element: <div>Contact page of react router</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
