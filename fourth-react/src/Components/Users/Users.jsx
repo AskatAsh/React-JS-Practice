8ec731c1ad08d7f46720906a8fcf328285a79022
AskatAsh <abdulaskat@gmail.com>
2023-11-06 14:54:07 +0600

    get API data using loader and fetch

--- fourth-react/src/Components/Users/Users.jsx
+++ fourth-react/src/Components/Users/Users.jsx
@@ -1,7 +1,10 @@
+import { useLoaderData } from "react-router-dom";
+
 const Users = () => {
+    const users = useLoaderData();
     return (
         <div>
-            <h2>Our Users</h2>
+            <h2>Our Users: {users.length}</h2>
             <p>This is the Users page of react routing website</p>
         </div>
     );
