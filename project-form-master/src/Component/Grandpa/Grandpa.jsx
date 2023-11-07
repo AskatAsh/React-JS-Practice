b17f8a883c54a5ba52ac8c57b49dc258e84c435b
AskatAsh <abdulaskat@gmail.com>
2023-11-08 01:21:31 +0600

    created context and used provider

--- project-form-master/src/Component/Grandpa/Grandpa.jsx
+++ project-form-master/src/Component/Grandpa/Grandpa.jsx
@@ -1,18 +1,23 @@
+import { createContext } from 'react';
 import Aunt from '../Aunt/Aunt';
 import Dad from '../Dad/Dad';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
+
+const AssetContext = createContext("Gold");
 const Grandpa = () => {
     const ring = "Diamond";
     return (
         <div className="grandpa">
             <div>
                 <h2>Grandpa</h2>
-                <section className='flex'>
-                    <Dad ring={ring}></Dad>
-                    <Aunt ring={ring}></Aunt>
-                    <Uncle></Uncle>
-                </section>
+                <AssetContext.Provider value='Gold'>
+                    <section className='flex'>
+                        <Dad ring={ring}></Dad>
+                        <Aunt ring={ring}></Aunt>
+                        <Uncle></Uncle>
+                    </section>
+                </AssetContext.Provider>
             </div>
         </div>
     );
