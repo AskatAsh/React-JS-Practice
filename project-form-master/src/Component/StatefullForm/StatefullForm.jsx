f6a20c30c72455d507644a77b5c194f66df0584f
AskatAsh <abdulaskat@gmail.com>
2023-11-07 13:12:08 +0600

    handled error using state and showed error message

--- project-form-master/src/Component/StatefullForm/StatefullForm.jsx
+++ project-form-master/src/Component/StatefullForm/StatefullForm.jsx
@@ -4,11 +4,19 @@ const StatefullForm = () => {
     const [name, setName] = useState(null);
     const [email, setEmail] = useState(null);
     const [phone, setPhone] = useState(null);
+    const [error, setError] = useState('');
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log("Form submitted");
-        console.log(name, email, phone);
+        // console.log("Form submitted");
+        if(phone.length < 11){
+            setError("Phone number must be 11 characters long!");
+        }
+        else{
+            setError('');
+            console.log(name, email, phone);
+        }
+        
     }
 
     const handleNameChange = e => {
@@ -38,6 +46,9 @@ const StatefullForm = () => {
                 <br />
                 <input type="submit" value="Submit" />
             </form>
+            {
+                error && <p>{error}</p>
+            }
         </div>
     );
 };
