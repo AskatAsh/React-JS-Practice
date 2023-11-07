29fb8527911f878a7ecefc896e72a875d8c71816
AskatAsh <abdulaskat@gmail.com>
2023-11-08 01:37:16 +0600

    used useState with context API to update value

--- project-form-master/src/Component/Grandpa/Grandpa.jsx
+++ project-form-master/src/Component/Grandpa/Grandpa.jsx
@@ -1,23 +1,28 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import Aunt from '../Aunt/Aunt';
 import Dad from '../Dad/Dad';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css'
 
 export const AssetContext = createContext("Gold");
+export const MoneyContext = createContext(1000);
 const Grandpa = () => {
+    const [money, setMoney] = useState(1000);
     const ring = "Diamond";
     return (
         <div className="grandpa">
             <div>
                 <h2>Grandpa</h2>
-                <AssetContext.Provider value='Gold'>
-                    <section className='flex'>
-                        <Dad ring={ring}></Dad>
-                        <Aunt ring={ring}></Aunt>
-                        <Uncle></Uncle>
-                    </section>
-                </AssetContext.Provider>
+                <p>Total: {money}</p>
+                <MoneyContext.Provider value={[money, setMoney]}>
+                    <AssetContext.Provider value='Gold'>
+                        <section className='flex'>
+                            <Dad ring={ring}></Dad>
+                            <Aunt ring={ring}></Aunt>
+                            <Uncle></Uncle>
+                        </section>
+                    </AssetContext.Provider>
+                </MoneyContext.Provider>
             </div>
         </div>
     );
--- project-form-master/src/Component/Uncle/Uncle.jsx
+++ project-form-master/src/Component/Uncle/Uncle.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import Cousin from "../Cousin/Cousin";
+import { MoneyContext } from "../Grandpa/Grandpa";
 
 const Uncle = () => {
+    const [money, setMoney] = useContext(MoneyContext);
     return (
         <div>
             <h2>Uncle</h2>
@@ -8,6 +11,8 @@ const Uncle = () => {
                 <Cousin cousin={"Amina"}></Cousin>
                 <Cousin cousin={"Jafor"}></Cousin>
             </section>
+            <p>Money: {money}</p>
+            <button onClick={() => setMoney(money + 1000)}>Add 1000 More</button>
         </div>
     );
 };
