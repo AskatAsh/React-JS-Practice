fe7795acac4bf78d562bcbdbe15c7ffd7ba523b7
AskatAsh <abdulaskat@gmail.com>
2023-11-08 21:09:59 +0600

    used template string to set img alt text to company name

--- project-career-hub/src/Components/Job/Job.jsx
+++ project-career-hub/src/Components/Job/Job.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
 const Job = ({job}) => {
-    const {logo} = job;
+    const {logo, company_name} = job;
     return (
         <div>
-            <img src={logo} alt="" />
+            <img src={logo} alt={`Image of ${company_name}`} />
         </div>
     );
 };
