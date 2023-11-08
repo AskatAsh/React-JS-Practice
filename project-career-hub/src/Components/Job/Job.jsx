b8b386f20c4bb91773d3f5c95a3bb7fcebc997ac
AskatAsh <abdulaskat@gmail.com>
2023-11-08 21:35:02 +0600

    added tailwind styles in jobs.jsx

--- project-career-hub/src/Components/Job/Job.jsx
+++ project-career-hub/src/Components/Job/Job.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import locationIcon from '../../assets/icons/Location2.png'
+import moneyIcon from '../../assets/icons/money.png'
 
 const Job = ({job}) => {
     const {logo, job_title, company_name, remote_or_onsite, job_type, location, salary} = job;
     return (
-        <div>
+        <div className='border-2 rounded-md p-10'>
             <img src={logo} alt={`Image of ${company_name}`} />
             <h2>{job_title}</h2>
             <p>{company_name}</p>
@@ -13,10 +15,10 @@ const Job = ({job}) => {
                 <span className='px-4 py-2 border-2 border-[#7E90FE] text-[#7E90FE] rounded font-semibold'>{job_type}</span>
             </div>
             <div className='flex gap-6'>
-                <p>{location}</p>
-                <p>Salary: {salary}</p>
+                <p className='flex items-center gap-2'><img src={locationIcon} alt="" />{location}</p>
+                <p className='flex items-center gap-2'><img src={moneyIcon} alt="" />Salary: {salary}</p>
             </div>
-            <Link className="text-lg font-bold btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white capitalize font-bold">View Details</Link>
+            <Link className="text-lg btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white capitalize font-bold">View Details</Link>
         </div>
     );
 };
