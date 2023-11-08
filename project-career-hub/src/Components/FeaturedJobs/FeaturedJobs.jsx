649752cbc06d112d9e5a8d8cce229bda4cd26d76
AskatAsh <abdulaskat@gmail.com>
2023-11-08 21:06:27 +0600

    used useEffect and map to get jobs data from jobs.json

--- project-career-hub/src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ project-career-hub/src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const title = "Featured Jobs";
     const subtitle = "Explore thousands of job opportunities with all the information you need. Its your future";
+
+    const [jobs, setJobs] = useState([]);
+    useEffect(() => {
+        fetch('jobs.json')
+        .then(res => res.json())
+        .then(data => setJobs(data))
+    }, []);
+
     return (
         <div>
             <SectionTitle title={title} subtitle={subtitle}></SectionTitle>
+            <p>Number of Jobs: {jobs.length}</p>
+            {
+                jobs.map(job => <Job key={job.id} job={job}></Job>)
+            }
         </div>
     );
 };
--- project-career-hub/src/Components/SectionTitle/SectionTitle.jsx
+++ project-career-hub/src/Components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const SectionTitle = ({title, subtitle}) => {
     return (
         <div>
-            <h2 className="text-center text-5xl font-extrabold mb-4 my-32">{title}</h2>
+            <h2 className="text-center text-5xl font-extrabold mb-4 mt-32">{title}</h2>
             <p className="text-[#757575] text-center">{subtitle}</p>
         </div>
     );
