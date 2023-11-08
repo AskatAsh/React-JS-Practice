5139617b6f52ae38fa41089313a7541465363c1e
AskatAsh <abdulaskat@gmail.com>
2023-11-08 21:27:00 +0600

    added data from jobs.json to jobs.jsx component

--- project-career-hub/src/Components/Job/Job.jsx
+++ project-career-hub/src/Components/Job/Job.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Job = ({job}) => {
-    const {logo, company_name} = job;
+    const {logo, job_title, company_name, remote_or_onsite, job_type, location, salary} = job;
     return (
         <div>
             <img src={logo} alt={`Image of ${company_name}`} />
+            <h2>{job_title}</h2>
+            <p>{company_name}</p>
+            <div className='flex gap-2'>
+                <span className='px-4 py-2 border-2 border-[#7E90FE] text-[#7E90FE] rounded font-semibold'>{remote_or_onsite}</span>
+                <span className='px-4 py-2 border-2 border-[#7E90FE] text-[#7E90FE] rounded font-semibold'>{job_type}</span>
+            </div>
+            <div className='flex gap-6'>
+                <p>{location}</p>
+                <p>Salary: {salary}</p>
+            </div>
+            <Link className="text-lg font-bold btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white capitalize font-bold">View Details</Link>
         </div>
     );
 };
