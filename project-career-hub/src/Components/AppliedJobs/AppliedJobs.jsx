174132179f0e06b4ec76516da42615bd76a1422e
AskatAsh <abdulaskat@gmail.com>
2023-11-09 20:50:19 +0600

    added dropdown filter icon

--- project-career-hub/src/Components/AppliedJobs/AppliedJobs.jsx
+++ project-career-hub/src/Components/AppliedJobs/AppliedJobs.jsx
@@ -9,19 +9,32 @@ const AppliedJobs = () => {
     const jobs = useLoaderData();
     useEffect(() => {
         const storedJobIds = getStoredJob();
-        if(jobs.length){
+        if (jobs.length) {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
             // console.log(jobsApplied);
             setAppliedJobs(jobsApplied);
         }
-    }, [])
+    }, [jobs])
 
     return (
-        <div className="flex flex-col gap-6 max-w-[1320px] mx-auto my-32">
-            {
-                appliedJobs.map(job => <AppliedJob key={job.id} job={job}></AppliedJob>)
-            }
+        <div className="max-w-[1320px] mx-auto my-32">
+            <div className="flex justify-end my-8">
+                <details className="dropdown">
+                    <summary className="m-1 btn">Filter By</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                </details>
+            </div>
+
+            <div className="flex flex-col gap-6">
+                {
+                    appliedJobs.map(job => <AppliedJob key={job.id} job={job}></AppliedJob>)
+                }
+            </div>
         </div>
+
     );
 };
 
