056dbfaba2383e9f6e3831c0ab3e154b2650de66
AskatAsh <abdulaskat@gmail.com>
2023-11-09 17:49:34 +0600

    added job experience requirements and responsibility

--- project-career-hub/src/Components/JobDetails/JobDetails.jsx
+++ project-career-hub/src/Components/JobDetails/JobDetails.jsx
@@ -6,7 +6,7 @@ const JobDetails = () => {
     // console.log(id, jobs);
     const foundJob = jobs.find(job => job.id === parseInt(id));
     // console.log(foundJob);
-    const { salary, job_description } = foundJob;
+    const { salary, job_description, job_responsibility, educational_requirements, experiences } = foundJob;
     return (
         <div>
             <div className="flex justify-center bg-[#faf8ff] py-28 text-[#1A1919]">
@@ -15,9 +15,20 @@ const JobDetails = () => {
 
             <section className="grid grid-cols-4 justify-center max-w-[1320px] mx-auto gap-6 my-32">
                 <div className="col-span-3 text-[#1A1919]">
-                    <p><span className="font-bold">Job Description: </span>{job_description}</p>
+                    <p className="text-[#474747] mb-6"><span className="font-bold text-[#1A1919]">Job Description: </span>{job_description}</p>
+                    <p className="text-[#474747] mb-6"><span className="font-bold text-[#1A1919]">Job Responsibility: </span>{job_responsibility}</p>
+                    <p className="text-[#474747] mb-6">
+                        <span className="font-bold text-[#1A1919]">Educational Requirements:</span>
+                        <br />
+                        <span>{educational_requirements}</span>
+                    </p>
+                    <p className="text-[#474747] mb-6">
+                        <span className="font-bold text-[#1A1919]">Experiences:</span>
+                        <br />
+                        <span>{experiences}</span>
+                    </p>
                 </div>
-                <div className="p-7 rounded-md" style={{background: "linear-gradient(90deg, rgba(126, 144, 254, 0.10) 0%, rgba(152, 115, 255, 0.10) 100%"}}>
+                <div className="p-7 rounded-md" style={{ background: "linear-gradient(90deg, rgba(126, 144, 254, 0.10) 0%, rgba(152, 115, 255, 0.10) 100%" }}>
                     <h2 className="text-xl font-extrabold">Job Details</h2>
                     <hr className="my-6" />
                     <p className="text-[#474747]"><span className="text-base font-semibold">Salaray: </span>{salary}</p>
