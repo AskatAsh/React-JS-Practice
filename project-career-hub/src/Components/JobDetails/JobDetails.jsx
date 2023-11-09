42a99592ab39c9d777b38fdfb7d5e74529f0630a
AskatAsh <abdulaskat@gmail.com>
2023-11-09 15:32:45 +0600

    used grid layout and showed job description of selected job

--- project-career-hub/src/Components/JobDetails/JobDetails.jsx
+++ project-career-hub/src/Components/JobDetails/JobDetails.jsx
@@ -1,22 +1,27 @@
-import { useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const JobDetails = () => {
-    const [loadedJobs, setLoadedJobs] = useState([]);
     const jobs = useLoaderData();
-    const {id} = useParams();
+    const { id } = useParams();
     // console.log(id, jobs);
-    {
-        const foundJob = jobs.find(job => job.id == id);
-        console.log(foundJob);
-        // setLoadedJobs(foundJob);
-    }
-    
+    const foundJob = jobs.find(job => job.id === parseInt(id));
+    // console.log(foundJob);
+    const { job_description } = foundJob;
     return (
         <div>
             <div className="flex justify-center bg-[#faf8ff] py-28">
                 <h2 className="text-4xl font-extrabold">Job Details</h2>
             </div>
+
+            <section className="grid grid-cols-4 justify-center max-w-[1320px] mx-auto gap-6 my-32">
+                <div className="col-span-3">
+                    <p><span className="font-bold">Job Description: </span>{job_description}</p>
+                </div>
+                <div className="">
+                    <h2>Job Details</h2>
+                </div>
+            </section>
+
         </div>
     );
 };
