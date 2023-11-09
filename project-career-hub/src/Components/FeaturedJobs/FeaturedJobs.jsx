a813f9f209d8a3bd4a110c0aeb12d5e90cb13c77
AskatAsh <abdulaskat@gmail.com>
2023-11-09 13:13:43 +0600

    used state and slice method to show only 4 jobs

--- project-career-hub/src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ project-career-hub/src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -6,6 +6,7 @@ const FeaturedJobs = () => {
     const title = "Featured Jobs";
     const subtitle = "Explore thousands of job opportunities with all the information you need. Its your future";
 
+    const [showJobs, setShowJobs] = useState(4);
     const [jobs, setJobs] = useState([]);
     useEffect(() => {
         fetch('jobs.json')
@@ -16,13 +17,15 @@ const FeaturedJobs = () => {
     return (
         <div className="px-5">
             <SectionTitle title={title} subtitle={subtitle}></SectionTitle>
-            <p>Number of Jobs: {jobs.length}</p>
-            <div className="grid justify-center gap-5 mt-8" style={{gridTemplateColumns: "repeat(auto-fit, minmax(300px, 648px))"}}>
+            {/* <p>Number of Jobs: {jobs.length}</p> */}
+            <div className="grid justify-center gap-5 mt-8" style={{ gridTemplateColumns: "repeat(auto-fit, minmax(300px, 648px))" }}>
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, showJobs).map(job => <Job key={job.id} job={job}></Job>)
                 }
             </div>
-
+            <div className={`flex justify-center my-10 ${showJobs == jobs.length ? 'hidden' : ''}`}>
+                <button className= "text-lg btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white capitalize font-bold">Show All Jobs</button>
+            </div>
         </div>
     );
 };
