78214c38ba451742ff40d2e261461bdb19f7c53d
AskatAsh <abdulaskat@gmail.com>
2023-11-09 15:14:38 +0600

    used useLoaderData and useParams and find method to find job

--- project-career-hub/src/Components/JobDetails/JobDetails.jsx
+++ project-career-hub/src/Components/JobDetails/JobDetails.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const JobDetails = () => {
+    const [loadedJobs, setLoadedJobs] = useState([]);
+    const jobs = useLoaderData();
+    const {id} = useParams();
+    // console.log(id, jobs);
+    {
+        const foundJob = jobs.find(job => job.id == id);
+        console.log(foundJob);
+        // setLoadedJobs(foundJob);
+    }
+    
     return (
         <div>
             <div className="flex justify-center bg-[#faf8ff] py-28">
