616a210e86c72eaf1ca53f4aed0d921ef0c9bb3c
AskatAsh <abdulaskat@gmail.com>
2023-11-09 21:35:59 +0600

    show all remote onsite data using filter and click handler

--- project-career-hub/src/Components/AppliedJobs/AppliedJobs.jsx
+++ project-career-hub/src/Components/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,7 @@ import downArrow from '../../assets/icons/downArrow.svg'
 
 const AppliedJobs = () => {
     const [appliedJobs, setAppliedJobs] = useState([]);
+    const [filterData, setFilterData] = useState([]);
 
     const jobs = useLoaderData();
     useEffect(() => {
@@ -14,24 +15,52 @@ const AppliedJobs = () => {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
             // console.log(jobsApplied);
             setAppliedJobs(jobsApplied);
+            setFilterData(jobsApplied);
         }
     }, [jobs])
+
+
+    const handlefilterData = (filter) => {
+        if (filter === "all") {
+            console.log("Selected All");
+            const allApplied = appliedJobs.map(job => job);
+            setFilterData(allApplied);
+            console.log(allApplied);
+        }
+        else if (filter === "remote") {
+            console.log("Selected Remote");
+            const remoteApplied = appliedJobs.filter(job => job.remote_or_onsite === "Remote");
+            setFilterData(remoteApplied);
+            console.log(remoteApplied);
+        }
+        else if (filter === "onSite") {
+            console.log("Selected onSite");
+            const onsiteApplied = appliedJobs.filter(job => job.remote_or_onsite === "Onsite");
+            setFilterData(onsiteApplied);
+            console.log(onsiteApplied);
+
+        }
+
+        console.log(filterData);
+    }
 
     return (
         <div className="max-w-[1320px] mx-auto my-32">
             <div className="flex justify-end my-8">
                 <details className="dropdown">
-                    <summary className="m-1 btn">Filter By {<img src={downArrow} alt="" className="w-4"/>}</summary>
+                    <summary className="m-1 btn">Filter By {<img src={downArrow} alt="" className="w-4" />}</summary>
                     <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 2</a></li>
+                        <li><a onClick={() => handlefilterData("all")}>All</a></li>
+                        <li><a onClick={() => handlefilterData("remote")}>Remote</a></li>
+                        <li><a onClick={() => handlefilterData("onSite")}>On Site</a></li>
                     </ul>
                 </details>
             </div>
 
             <div className="flex flex-col gap-6">
                 {
-                    appliedJobs.map(job => <AppliedJob key={job.id} job={job}></AppliedJob>)
+                    // appliedJobs.map(job => <AppliedJob key={job.id} job={job}></AppliedJob>)
+                    filterData.map(job => <AppliedJob key={job.id} job={job}></AppliedJob>)
                 }
             </div>
         </div>
