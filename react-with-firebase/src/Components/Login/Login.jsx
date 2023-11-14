17c8c24acf1867332bf851814da12c33cf1dce5d
AskatAsh <abdulaskat@gmail.com>
2023-11-14 18:55:04 +0600

    imported firbase authantication and used getAuth

--- react-with-firebase/src/Components/Login/Login.jsx
+++ react-with-firebase/src/Components/Login/Login.jsx
@@ -1,12 +1,15 @@
+import { getAuth } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 
 const Login = () => {
+    const auth = getAuth(app);
 
     const handleLogin = () => {
         console.log("clicked the login button");
     }
     return (
         <div style={{marginTop: "40px"}}>
-            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogin}>Google Login</button>
         </div>
     );
 };
