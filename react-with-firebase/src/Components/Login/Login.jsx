9737e560755e2258bce9f6c81d82bb9e5be1aca8
AskatAsh <abdulaskat@gmail.com>
2023-11-14 19:46:31 +0600

    showed google login info using useState

--- react-with-firebase/src/Components/Login/Login.jsx
+++ react-with-firebase/src/Components/Login/Login.jsx
@@ -1,23 +1,33 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app);
     // console.log(app);
     const provider = new GoogleAuthProvider();
 
     const handleGoogleLogin = () => {
         signInWithPopup(auth, provider)
-        .then((result) => {
-            const user = result.user;
-            console.log(user);
-        }).catch((error) => {
-            console.log("Error: ", error.message);
-        })
+            .then((result) => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
+            }).catch((error) => {
+                console.log("Error: ", error.message);
+            })
     }
     return (
-        <div style={{marginTop: "40px"}}>
-            <button onClick={handleGoogleLogin}>Google Login</button>
+        <div>
+            <button onClick={handleGoogleLogin} style={{ margin: "40px 0px" }}>Google Login</button>
+            {user &&
+                <div>
+                    <img src={user?.photoURL} alt="" />
+                    <h3>Name: {user.displayName}</h3>
+                    <p>Email: {user.email}</p>
+                </div>
+            }
         </div>
     );
 };
