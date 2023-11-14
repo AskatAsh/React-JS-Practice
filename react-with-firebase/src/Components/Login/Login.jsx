8d199cd94a5159a1788caed703a0ed0add646951
AskatAsh <abdulaskat@gmail.com>
2023-11-14 19:22:04 +0600

    added google login using google auth provider from firebase

--- react-with-firebase/src/Components/Login/Login.jsx
+++ react-with-firebase/src/Components/Login/Login.jsx
@@ -1,15 +1,23 @@
-import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 
 const Login = () => {
     const auth = getAuth(app);
+    // console.log(app);
+    const provider = new GoogleAuthProvider();
 
-    const handleLogin = () => {
-        console.log("clicked the login button");
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            console.log(user);
+        }).catch((error) => {
+            console.log("Error: ", error.message);
+        })
     }
     return (
         <div style={{marginTop: "40px"}}>
-            <button onClick={handleLogin}>Google Login</button>
+            <button onClick={handleGoogleLogin}>Google Login</button>
         </div>
     );
 };
