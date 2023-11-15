9e039f1d14b95e8822b1a8443aed3217144fecae
AskatAsh <abdulaskat@gmail.com>
2023-11-15 21:11:53 +0600

    used signInWithEmailAndPassword to log in using firebase

--- login-register/src/Components/Login/Login.jsx
+++ login-register/src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
@@ -16,78 +16,67 @@ const Login = () => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        const accepted = e.target.terms.checked
-        console.log(email, password, accepted);
+        console.log(email, password);
 
-        if (password.length < 6) {
-            setErrorMessage("Password should be at least 6 characters long.");
-            return;
-        }
-        else if (!/(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9_]).*/.test(password)) {
-            setErrorMessage("Password must contain at least one digit, uppercase letter and special character.");
-            return;
-        }
-
-        if(!accepted){
-            setErrorMessage("Please Accept Our Terms and Conditions");
-            return;
-        }
         setErrorMessage('');
         setSuccess('');
 
-        createUserWithEmailAndPassword(auth, email, password)
+        signInWithEmailAndPassword(auth, email, password)
             .then(result => {
                 console.log(result);
-                setSuccess("Your Information has been Successfully Registered.");
+                setSuccess("Your Have Successfully logged in.");
             }).catch(error => {
-                setErrorMessage(error.code);
-                console.log(error.code);
+                setErrorMessage("Your Email or Password is Incorrect..!");
+                console.error(error);
             })
     }
     return (
         <div>
-        <div className="hero">
-            <div className="hero-content flex-col items-center py-10">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6 max-w-xl">Login with email and password using firbase</p>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body" onSubmit={handleLogin}>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input name="email" type="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <div className="flex items-center relative">
-                                <input name="password" type={showPassword ? "text" : "password"}
-                                    placeholder="password" className="input input-bordered flex-1" required />
-                                <span onClick={handleShowPassword} className="text-xl p-2 absolute right-2 rounded-full hover:bg-slate-700 ">
-                                    {
-                                        showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
-                                    }
-                                </span>
+            <div className="hero">
+                <div className="hero-content flex-col items-center py-10">
+                    <div className="text-center">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6 max-w-xl">Login with email and password using firbase</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body" onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input name="email" type="email" placeholder="email" className="input input-bordered" required />
                             </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <div className="flex items-center relative">
+                                    <input name="password" type={showPassword ? "text" : "password"}
+                                        placeholder="password" className="input input-bordered flex-1" required />
+                                    <span onClick={handleShowPassword} className="text-xl p-2 absolute right-2 rounded-full hover:bg-slate-700 ">
+                                        {
+                                            showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                                        }
+                                    </span>
+                                </div>
+                                <label className="label my-2">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
 
-                            <label className="label text-xs">
-                                {errorMessage ? <span className="text-red-400">{errorMessage}</span>
-                                    : <span className="text-green-400">{success}</span>}
-                            </label>
-                            
-                        </div>
-                        <div className="form-control mt-6">
-                            <input type="submit" value="Login" className="btn btn-primary" />
-                        </div>
-                    </form>
+                                <label className="label text-xs">
+                                    {errorMessage ? <span className="text-red-400">{errorMessage}</span>
+                                        : <span className="text-green-400">{success}</span>}
+                                </label>
+
+                            </div>
+                            <div className="form-control mt-3">
+                                <input type="submit" value="Login" className="btn btn-primary" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
