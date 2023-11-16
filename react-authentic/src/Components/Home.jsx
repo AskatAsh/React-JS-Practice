d5a7141f75af33d12786d447622c77a8263749a5
AskatAsh <abdulaskat@gmail.com>
2023-11-17 01:37:43 +0600

    used context API to pass full app as children

--- react-authentic/src/Components/Home.jsx
+++ react-authentic/src/Components/Home.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "../Context/AuthProvider";
 
 const Home = () => {
+    const authInfo = useContext(AuthContext)
+
     return (
         <div>
-            <h1 className='text-4xl font-bold'>Authentication using React and Firebase.</h1>
+            <h1 className='text-4xl font-bold'>{authInfo.name}</h1>
         </div>
     );
 };
--- react-authentic/src/main.jsx
+++ react-authentic/src/main.jsx
@@ -9,6 +9,7 @@ import {
 import Home from './Components/Home.jsx';
 import Register from './Components/Register.jsx';
 import Login from './Components/Login.jsx';
+import AuthProvider from './Context/AuthProvider.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -32,6 +33,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
