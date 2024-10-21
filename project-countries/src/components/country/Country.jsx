33b2c64f6757f61efa7b36281d56b0229a0b7d1e
AskatAsh <abdulaskat@gmail.com>
2024-10-22 00:32:55 +0600

    used css to style img in container

--- project-countries/src/components/country/Country.jsx
+++ project-countries/src/components/country/Country.jsx
@@ -1,31 +1,48 @@
-import { useState } from 'react';
-import './Country.css';
+import { useState } from "react";
+import "./Country.css";
 
 const Country = ({ country, handleVisitedCountry }) => {
+  const { name, flags, capital, population, languages } = country;
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+    handleVisitedCountry(country);
+  };
 
-    const { name, flags, capital, population, languages } = country;
-    const [visited, setVisited] = useState(false);
-    const handleVisited = () => {
-        setVisited(!visited);
-        handleVisitedCountry(country);
-    }
+  const language = !languages ? ["Not Available"] : Object.values(languages);
 
-    const language = !languages ? ["Not Available"] : Object.values(languages);
+  return (
+    <div className={`country ${visited && "visited"}`}>
+      <h3>Name: {name?.common}</h3>
+      <div className="img-container">
+        <img src={flags.png} alt={flags.alt} className="flag" />
+      </div>
+      <p>
+        <b>Capital:</b> {capital}, <b>Population:</b> {population}
+      </p>
+      <p>
+        <b>Language: </b>
+        {language.map((lang, id) => (
+          <span key={id}>
+            {lang}
+            {id == language.length - 1 ? "" : ", "}
+          </span>
+        ))}
+      </p>
 
-    return (
-        <div className={`country ${visited && 'visited'}`}>
-            <h3>Name: {name?.common}</h3>
-            <img src={flags.png} alt={flags.alt} className='flag' />
-            <p><b>Capital:</b> {capital}, <b>Population:</b> {population}</p>
-            <p><b>Language: </b>{
-                language.map((lang, id) => <span key={id}>{lang}{(id == language.length-1) ? "": ", "}</span>)
-            }</p>
-
-            <div className='flex-button'>
-                <button onClick={handleVisited} disabled={visited}>{visited ? <span>Visited <i className="fa-solid fa-circle-check"></i></span> : "Travel"}</button>
-            </div>
-        </div>
-    );
+      <div className="flex-button">
+        <button onClick={handleVisited} disabled={visited}>
+          {visited ? (
+            <span>
+              Visited <i className="fa-solid fa-circle-check"></i>
+            </span>
+          ) : (
+            "Travel"
+          )}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Country;
